c618c828908784edd2bbee6741f3b8407aff555f
dshkalikov <bern1991@gmail.com>
2022-05-30 17:43:43 +0300

    First task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo  = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanTwo.getMessage());
+        Cat catOne = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+        boolean compateHW = bean == beanTwo;
+        System.out.println(compateHW);
+        boolean compareCats = catOne == catTwo;
+        System.out.println(compareCats);
+
+
     }
 }
